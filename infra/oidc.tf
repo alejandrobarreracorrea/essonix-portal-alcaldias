775e5fc720fd9b521e72d1bb45c7a888d208b2d6
Alejandro <alejandro.barrera.correa@gmail.com>
2026-06-30 18:52:41 -0500

    fix(infra): harden GitHub OIDC (2nd thumbprint, StringEquals sub, drop unused data source)

--- infra/oidc.tf
+++ infra/oidc.tf
@@ -1,9 +1,10 @@
-data "aws_caller_identity" "me" {}
-
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ]
 }
 
 data "aws_iam_policy_document" "trust" {
@@ -19,7 +20,7 @@ data "aws_iam_policy_document" "trust" {
       values   = ["sts.amazonaws.com"]
     }
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
       values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
     }
